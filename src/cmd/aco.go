be4b3980f580535e8044e7c2200935690a783cca
heitorfreitasferreira <heitor.ff@hotmail.com>
2025-03-18 19:37:56 -0300

    feat: aco cmd

--- src/cmd/aco.go
+++ src/cmd/aco.go
@@ -1,11 +1,14 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"math/rand"
+	"tcc/aco"
+	"tcc/graph"
+	"tcc/shared"
 
 	"github.com/spf13/cobra"
 )
@@ -17,24 +20,93 @@ var acoCmd = &cobra.Command{
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
-Cobra is a CLI library for Go that empowers applications.
+Cobra is a CLI library for Go that empoalphaers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("aco called")
+		seed, err := cmd.Root().PersistentFlags().GetInt64("seed")
+		if err != nil {
+			cmd.PrintErrln("Error getting random seed parameter:", err)
+			return
+		}
+		instance, err := cmd.Parent().PersistentFlags().GetString("instance")
+		if err != nil {
+			cmd.PrintErrln("Error getting instance file path:", err)
+			return
+		}
+
+		rng := rand.New(rand.NewSource(seed))
+		graph, err := graph.LoadFromFile(instance)
+		if err != nil {
+			cmd.PrintErrln("Error getting folder:", err)
+			return
+		}
+		params, err := getAcoParams(cmd)
+		if err != nil {
+			cmd.PrintErrln("Error getting parameters: ", err)
+			return
+		}
+		fmt.Println(aco.Optimize(params, graph, rng))
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(acoCmd)
+	optimizeCmd.AddCommand(acoCmd)
 
-	// Here you will define your flags and configuration settings.
+	acoCmd.Flags().Float64("alpha", 1.0, "Alpha")
+	acoCmd.Flags().Float64("beta", 2.0, "Beta")
+	acoCmd.Flags().Float64("gama", 0.1, "Gama")
+	acoCmd.Flags().Float64("rho", 0.5, "Rho")
+	acoCmd.Flags().Float64("q", 100, "Q")
+}
 
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// acoCmd.PersistentFlags().String("foo", "", "A help for foo")
+func getAcoParams(cmd *cobra.Command) (aco.Params, error) {
+	iterations, err := cmd.Parent().PersistentFlags().GetInt("iterations")
+	if err != nil {
+		cmd.PrintErrln("Error getting iterations parameter:", err)
+		return aco.Params{}, err
+	}
+	population, err := cmd.Parent().PersistentFlags().GetInt("population")
+	if err != nil {
+		cmd.PrintErrln("Error getting population parameter:", err)
+		return aco.Params{}, err
+	}
 
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// acoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	alpha, err := cmd.Flags().GetFloat64("alpha")
+	if err != nil {
+		cmd.PrintErrln("Error getting alpha", err)
+		return aco.Params{}, err
+	}
+	beta, err := cmd.Flags().GetFloat64("beta")
+	if err != nil {
+		cmd.PrintErrln("Error getting beta", err)
+		return aco.Params{}, err
+	}
+	gama, err := cmd.Flags().GetFloat64("gama")
+	if err != nil {
+		cmd.PrintErrln("Error getting gama", err)
+		return aco.Params{}, err
+	}
+	rho, err := cmd.Flags().GetFloat64("rho")
+	if err != nil {
+		cmd.PrintErrln("Error getting Rho", err)
+		return aco.Params{}, err
+	}
+
+	q, err := cmd.Flags().GetFloat64("q")
+	if err != nil {
+		cmd.PrintErrln("Error getting Q", err)
+		return aco.Params{}, err
+	}
+	return aco.Params{
+		HyperParams: shared.HyperParams{
+			Iterations:     iterations,
+			PopulationSize: population,
+		},
+		Alpha: alpha,
+		Beta:  beta,
+		Gama:  gama,
+		Rho:   rho,
+		Q:     q,
+	}, nil
 }
