9d33723d862557db38366f7f594425ccb0c31dfa
heitorfreitasferreira <heitor.ff@hotmail.com>
2025-04-15 20:06:24 -0300

    refactor pra rodar varios metodos ao inves de somente bruteforce

---
+++ src/run_all.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  --frequency=*)
+    frequency_input="${1#*=}"
+    ;;
+  --method=*)
+    optimization_method="${1#*=}"
+    ;;
+  *)
+    echo "Unknown option: $1"
+    exit 1
+    ;;
+  esac
+  shift
+done
+
+frequency_input="${frequency_input:-10:3,11:3,12:3,13:3,14:3}"
+optimization_method="${optimization_method:-bruteforce}"
+
+./tcc create --frequency "${frequency_input}"
+
+shopt -s nullglob
+files=(data/*.graph)
+
+for file in "${files[@]##*/}"; do
+  base="${file%.graph}"
+  echo "./tcc optimize ${optimization_method} --instance data/${file} > data/${base}.${optimization_method}"
+done | parallel -j $(nproc)
